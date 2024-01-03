c90273208c66b0797a264d024695943aae6d4b5b
nms200299 <nms200299@naver.com>
2024-01-03 19:37:49 +0900

    chkIface 작업

---
+++ def_chkIface.py
@@ -0,0 +1,14 @@
+import os
+
+def chkIface():
+    if (os.path.exists('./interface.txt')):
+        file = open('./interface.txt', 'r')
+        iface = file.readline()
+        # 파일이 존재하면 읽음
+    else:
+        iface = input("Input your interface : ")
+        file = open('./interface.txt', 'w')
+        file.write(iface)
+        # 파일이 존재하지 않으면 생성
+    file.close()
+    return iface
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-adasd
-ddddd
+
+from def_chkIface import chkIface
+
+print('1. DeAuth Attack')
+print('2. EAPOL Collect')
+
+print('!'+chkIface()+'!')
